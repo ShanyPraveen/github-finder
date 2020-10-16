0b192270ec7c9b45d5853fe950e6dad3b5303afc
Shany Praveen <praveens@theecode.xyz>
2020-10-17 00:32:52 +0530

    seaech users works

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import NavBar from './components/layout/Navbar'
 import './App.css';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import axios from 'axios'
+import PropTypes from 'prop-types'
 
 class App extends Component {
   state = {
@@ -16,10 +18,20 @@ class App extends Component {
   //   })
   // }
 
-  async componentDidMount(){
+  // async componentDidMount(){
+  //   this.setState({ loading: true })
+  //   const res = await axios.get(`https://api.github.com/users?client_secret=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+  //   this.setState({  users: res.data, loading: false})
+  // }
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+  }
+
+  searchUsers = async (text) => {
     this.setState({ loading: true })
-    const res = await axios.get('https://api.github.com/users')
-    this.setState({ loading: false, users: res.data})
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    this.setState({  users: res.data.items, loading: false})
   }
 
   render(){
@@ -29,6 +41,7 @@ class App extends Component {
           <NavBar />
         </nav>
         <div className="container">
+          <Search searchUsers={this.searchUsers}/>
         <Users loading={this.state.loading} users={this.state.users}/>
         </div>
       </div>
